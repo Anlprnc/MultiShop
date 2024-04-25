f262828698302ef4589513c0e7da60643c178ee4
Anlprnc <anilpirincciii@gmail.com>
2024-04-25 23:05:39 +0300

    duendo

--- IdentityServer/MultiShop.IdentityServer/SeedData.cs
+++ IdentityServer/MultiShop.IdentityServer/SeedData.cs
@@ -1,107 +1,84 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using System;
-using System.Linq;
-using System.Security.Claims;
-using IdentityModel;
-using MultiShop.IdentityServer.Data;
-using MultiShop.IdentityServer.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 using Serilog;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Duende.IdentityServer.Models;
 
-namespace MultiShop.IdentityServer
+namespace MultiShop.IdentityServer;
+
+public class SeedData
 {
-    public class SeedData
+    public static void EnsureSeedData(WebApplication app)
     {
-        public static void EnsureSeedData(string connectionString)
+        using (var scope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
         {
-            var services = new ServiceCollection();
-            services.AddLogging();
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlite(connectionString));
+            scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+            context.Database.Migrate();
+            EnsureSeedData(context);
+        }
+    }
 
-            using (var serviceProvider = services.BuildServiceProvider())
+    private static void EnsureSeedData(ConfigurationDbContext context)
+    {
+        if (!context.Clients.Any())
+        {
+            Log.Debug("Clients being populated");
+            foreach (var client in Config.Clients.ToList())
             {
-                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
-                {
-                    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
-                    context.Database.Migrate();
-
-                    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-                    var alice = userMgr.FindByNameAsync("alice").Result;
-                    if (alice == null)
-                    {
-                        alice = new ApplicationUser
-                        {
-                            UserName = "alice",
-                            Email = "AliceSmith@email.com",
-                            EmailConfirmed = true,
-                        };
-                        var result = userMgr.CreateAsync(alice, "Pass123$").Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-
-                        result = userMgr.AddClaimsAsync(alice, new Claim[]{
-                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Alice"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
-                        }).Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-                        Log.Debug("alice created");
-                    }
-                    else
-                    {
-                        Log.Debug("alice already exists");
-                    }
-
-                    var bob = userMgr.FindByNameAsync("bob").Result;
-                    if (bob == null)
-                    {
-                        bob = new ApplicationUser
-                        {
-                            UserName = "bob",
-                            Email = "BobSmith@email.com",
-                            EmailConfirmed = true
-                        };
-                        var result = userMgr.CreateAsync(bob, "Pass123$").Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-
-                        result = userMgr.AddClaimsAsync(bob, new Claim[]{
-                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
-                            new Claim(JwtClaimTypes.GivenName, "Bob"),
-                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
-                            new Claim("location", "somewhere")
-                        }).Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-                        Log.Debug("bob created");
-                    }
-                    else
-                    {
-                        Log.Debug("bob already exists");
-                    }
-                }
+                context.Clients.Add(client.ToEntity());
             }
+            context.SaveChanges();
+        }
+        else
+        {
+            Log.Debug("Clients already populated");
+        }
+
+        if (!context.IdentityResources.Any())
+        {
+            Log.Debug("IdentityResources being populated");
+            foreach (var resource in Config.IdentityResources.ToList())
+            {
+                context.IdentityResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+        else
+        {
+            Log.Debug("IdentityResources already populated");
+        }
+
+        if (!context.ApiScopes.Any())
+        {
+            Log.Debug("ApiScopes being populated");
+            foreach (var resource in Config.ApiScopes.ToList())
+            {
+                context.ApiScopes.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+        else
+        {
+            Log.Debug("ApiScopes already populated");
+        }
+
+        if (!context.IdentityProviders.Any())
+        {
+            Log.Debug("OIDC IdentityProviders being populated");
+            context.IdentityProviders.Add(new OidcProvider
+            {
+                Scheme = "demoidsrv",
+                DisplayName = "IdentityServer",
+                Authority = "https://demo.duendesoftware.com",
+                ClientId = "login",
+            }.ToEntity());
+            context.SaveChanges();
+        }
+        else
+        {
+            Log.Debug("OIDC IdentityProviders already populated");
         }
     }
 }
